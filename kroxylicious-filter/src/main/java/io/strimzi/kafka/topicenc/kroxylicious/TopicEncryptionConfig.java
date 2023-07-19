224f2883ba6b2fdd8d7336bb9d3c8a6f38ea5c9b
Sam Barker <sbarker@redhat.com>
2023-07-20 11:47:04 +1200

    Context cacheLoader can never help us as we can't trigger loading from a cache miss directly.

--- kroxylicious-filter/src/main/java/io/strimzi/kafka/topicenc/kroxylicious/TopicEncryptionConfig.java
+++ kroxylicious-filter/src/main/java/io/strimzi/kafka/topicenc/kroxylicious/TopicEncryptionConfig.java
@@ -1,16 +1,11 @@
 package io.strimzi.kafka.topicenc.kroxylicious;
 
-import java.time.Duration;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
-
-import org.apache.kafka.common.Uuid;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 
 import io.strimzi.kafka.topicenc.policy.PolicyRepository;
 
@@ -23,17 +18,12 @@ public class TopicEncryptionConfig extends BaseConfig {
 
     @JsonIgnore
     private static final ConcurrentHashMap<String, TopicIdCache> virtualClusterToTopicUUIDToTopicNameCache = new ConcurrentHashMap<>();
-    private final ContextCacheLoader contextCacheLoader = new ContextCacheLoader();
 
     @JsonCreator
     public TopicEncryptionConfig(@JsonProperty(value = IN_MEMORY_POLICY_REPOSITORY_PROP_NAME) InMemoryPolicyRepositoryConfig inMemoryPolicyRepository) {
         this.inMemoryPolicyRepository = inMemoryPolicyRepository;
         Objects.requireNonNull(inMemoryPolicyRepository, "Currently " + IN_MEMORY_POLICY_REPOSITORY_PROP_NAME
                 + " configuration is required as it is the only PolicyRepository implementation");
-    }
-
-    public ContextCacheLoader getContextCacheLoader() {
-        return contextCacheLoader;
     }
 
     public PolicyRepository getPolicyRepository() {
